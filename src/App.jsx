a24a2febac72d97587e4067fb3bc29f29c198d5e
zaid-sinpie <zaidmasuldar@gmail.com>
2024-04-06 16:18:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar.jsx';
+
+function App() {
+
+  return (
+    <>
+     <Navbar></Navbar>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import logo from '../assets/logo.png'
+export default function Navbar({}){
+    return(
+        <ul className='navbar'>
+            <div className="logo"><img src={logo} alt="" /></div>
+            <div className="btns">
+                <button className='btn'>Login</button>
+                <button className='btn'>Signup</button>
+            </div>
+        </ul>
+    )
+}
