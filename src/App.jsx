853ff60e27ed69c2217d3d1bbd3d8a81c929b963
zaid-sinpie <zaidmasuldar@gmail.com>
2024-04-08 00:19:40 +0530

    btn hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import './App.css';
-import Navbar from './components/Navbar.jsx';
-import Section from './components/Section.jsx';
-import Content from './components/Content.jsx';
+import "./App.css";
+import Navbar from "./components/Navbar.jsx";
+import Section from "./components/Section.jsx";
+import Content from "./components/Content.jsx";
 
 function App() {
-
   return (
     <>
-     <Navbar />
-     <Section>
-     <Content />
-     </Section>
+      <Navbar />
+      <Section>
+        <Content />
+      </Section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
