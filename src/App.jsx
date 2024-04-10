cb5687e182dc5270636ec263b2303b8408fa664f
zaid-sinpie <zaidmasuldar@gmail.com>
2024-04-10 15:14:48 +0530

    adjustedment

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 import Section from "./components/Section.jsx";
 import Content from "./components/Content.jsx";
-import LoginPop from "./components/LoginPop.jsx";
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Navbar />
       <Section>
         <Content />
-        {/* <LoginPop /> */}
       </Section>
     </>
   );
